dcf058b78cc9248e1ff26186e51559f261e7ff41
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-05-16 12:13:52 -0400

    adding methods

--- main.go
+++ main.go
@@ -12,14 +12,19 @@ type Graph struct {
 
 // Node : Data type for nodes
 type Node struct {
-	Key      string
-	LinkedTo []*Node
+	Key        string
+	LinkedTo   []*Node
+	PathLength int
+	Prev       *Node
+	Temp       bool
 }
 
 // Edge : Data type for edges
 type Edge struct {
-	Start, End *Node
-	Cost       int
+	StartKey, EndKey string
+	Start, End       *Node
+	Cost             int
+	Visited          bool
 }
 
 // addNode : Adds a node to the graph
@@ -35,6 +40,21 @@ func (graph *Graph) addNode(newNode *Node) {
 	}
 	newArr[len(graph.Nodes)] = newNode
 	graph.Nodes = newArr
+}
+
+// addEdge : Adds a edges to the graph
+func (graph *Graph) addEdge(newEdge *Edge) {
+	newArr := make([]*Edge, len(graph.Edges)+1)
+	if len(graph.Edges) == 0 {
+		newArr[0] = newEdge
+		graph.Edges = newArr
+		return
+	}
+	for i := 0; i < len(graph.Edges); i++ {
+		newArr[i] = graph.Edges[i]
+	}
+	newArr[len(graph.Edges)] = newEdge
+	graph.Edges = newArr
 }
 
 // createNode : Creates and returns a new node
@@ -61,19 +81,70 @@ func (node *Node) createLink(link *Node) {
 	node.LinkedTo = newArr
 }
 
+// createEdges : Creates edges
+func createEdge(start, end *Node) *Edge {
+	edge := new(Edge)
+	edge = &Edge{
+		StartKey: start.Key,
+		EndKey:   end.Key,
+		Start:    start,
+		End:      end,
+	}
+	return edge
+}
+
+func (graph *Graph) obtainIndex(node *Node) int {
+	for i := 0; i < len(graph.Edges); i++ {
+		if graph.Edges[i].StartKey == node.Key {
+			return i
+		}
+	}
+	return -1
+}
+
+func (graph *Graph) lowestPath() (node *Node) {
+	minPath := graph.Nodes[0]
+	for i := 0; i < len(graph.Nodes); i++ {
+		if graph.Nodes[i].PathLength < minPath.PathLength {
+			minPath = graph.Nodes[i]
+		}
+	}
+	minPath.Temp = false
+	return minPath
+}
+
+func (graph *Graph) dijkstra(startNode, endNode *Node) {
+	for i := 0; i < len(graph.Nodes); i++ {
+		graph.Nodes[i].PathLength = 10000000
+		graph.Nodes[i].Prev = nil
+		graph.Nodes[i].Temp = true
+	}
+	graph.Nodes[graph.obtainIndex(startNode)].PathLength = 0
+	current := graph.lowestPath()
+	fmt.Println(current)
+}
+
 func main() {
 	graph := new(Graph)
-	arr := [7]string{"a", "b", "c", "d", "e", "f", "g"}
-	for i := 0; i < len(arr); i++ {
-		graph.addNode(createNode(arr[i]))
+	keys := [7]string{"a", "b", "c", "d", "e", "f", "g"}
+	costs := [9]int{2, 1, 1, 2, 5, 7, 6, 9, 8}
+	for i := 0; i < len(keys); i++ {
+		graph.addNode(createNode(keys[i]))
 	}
 	connections := [7][]int{{1, 6}, {2, 4, 3}, {4}, {4}, {5}, {-1}, {3}}
 	for i := 0; i < len(graph.Nodes); i++ {
 		for j := 0; j < len(connections[i]); j++ {
 			if connections[i][j] > -1 {
 				graph.Nodes[i].createLink(graph.Nodes[connections[i][j]])
+				graph.addEdge(createEdge(graph.Nodes[i], graph.Nodes[connections[i][j]]))
 			}
 		}
 	}
-	fmt.Println(graph.Nodes[0].LinkedTo[0].Key)
+	for i := 0; i < len(graph.Edges); i++ {
+		graph.Edges[i].Cost = costs[i]
+	}
+	for i := 0; i < len(graph.Nodes); i++ {
+		fmt.Println(graph.Nodes[i])
+	}
+	graph.dijkstra(graph.Nodes[0], graph.Nodes[3])
 }
